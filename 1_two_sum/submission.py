e0c90bc20d5c474fa5e7f0f2eb412a7b985596a5
hjdoc <halleydocherty@gmail.com>
2022-11-17 20:43:21 +0000

    refactor faster approach

--- 1_two_sum/submission.py
+++ 1_two_sum/submission.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
 
-        open_list = list(enumerate(nums))
-        # brute force
-        while len(open_list) > 1:
-            first = open_list.pop(0)
-            for num in open_list:
-                if first[1] + num[1] == target:
-                    return [first[0], num[0]]
-
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in nums and nums.index(remaining) != i:
+                n = nums.index(remaining)
+                return [i, n]
