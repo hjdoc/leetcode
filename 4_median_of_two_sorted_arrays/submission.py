8cdbdb34c7dabd487a92ff9ecc2fd46118bf41ba
hjdoc <halleydocherty@gmail.com>
2022-11-21 20:36:03 +0000

    refactor for true float median

--- 4_median_of_two_sorted_arrays/submission.py
+++ 4_median_of_two_sorted_arrays/submission.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        return float(sum(nums1+nums2))/len(nums1+nums2)
-
+        merged_array = sorted(nums1+nums2)
+        if not len(merged_array) % 2 == 0:
+            # division rounds up odd len to find median index
+            return merged_array[len(merged_array)/2]
+        else:
+            # return mean of two middle nums
+            key = len(merged_array)/2
+            return (merged_array[key] + float(merged_array[key-1]))/2     
+        
